fd2978da540a69c31c84181912e62fbd25df4b3b
valteravila <valter-neto00@outlook.com>
2022-06-21 14:46:50 -0300

    Projeto banco finalizado.

---
+++ js-orientacao-objetos-aula4/Cliente.js
@@ -0,0 +1,18 @@
+export class Cliente{
+    nome;
+    _cpf;
+
+    get cpf(){
+        this._cpf;
+    }
+
+
+    constructor(nome, cpf){
+        this.nome = nome;
+        this._cpf = cpf;
+    }
+
+
+}
+
+
